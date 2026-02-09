cc3ec68fb061073f20c326b6630c953263f821c4
curt vance <c.vance7777@hotmail.com>
2026-02-09 16:32:12 -0500

    added code

--- books.js
+++ books.js
@@ -1,7 +1,13 @@
+let books;
+
 async function renderBooks(filter) {
   const booksWrapper = document.querySelector(`.books`);
-const books = await getBooks();
-console.log(books)
+  booksWrapper.classList += " books__loading"
+
+  if (!books){
+ books = await getBooks();
+}
+  booksWrapper.classList.remove("books__loading")
 
 if (filter === 'LOW__TO__HIGH') {
  books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
